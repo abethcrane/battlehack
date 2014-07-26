915669fe5c3d70ef3f97207c965aa63aaf4b26bc
Luke <dahlukeh@gmail.com>
2014-07-27 09:09:35 +1000

    Removed extra stuff

--- app-real/facebook/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/beth/Dropbox/Projects/facebook-android-sdk-3.15.0/facebook/bin/AndroidManifest.xml : \
-/Users/beth/Dropbox/Projects/facebook-android-sdk-3.15.0/facebook/AndroidManifest.xml \
